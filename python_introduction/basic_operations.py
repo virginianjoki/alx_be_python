b28500fd1b96e35214e48c963540643b23d02fee
virginianjoki <njokivirginia0619@gmail.com>
2024-09-14 13:18:39 +0300

    basic_operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+# sum 
+addition = number1 + number2
+print("Addition of", number1, "and", number2, "is",  addition)
+# subtraction
+subtraction = number1 - number2
+print("Subtraction of" ,number1, "and" ,number2, "is ",subtraction)
+#multiplication
+multiplication = number1 * number2
+print("Multiplication of" ,number1, "and" ,number2,"is" ,multiplication)
