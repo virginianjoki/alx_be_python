6d679b421d488f71e170335503e8922fe2b776b5
virginianjoki <njokivirginia0619@gmail.com>
2024-09-21 20:09:49 +0300

    daily_reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,16 +1,25 @@
-Task = input("enter your task:")
-Priority = input("high, medium, low:")
-Time_Bound = input("Is it time-bound? (yes/no):")
+task = input("enter your task:")
+priority = input("high/medim/low:")
+time_bound = input("Is it time-bound? (yes/no):")
+
 match priority:
     case "high":
-         reminder = (f"{task}is a high priority task ")
+        priority_message = "high priority"
+        if time_bound == "yes":
+            reminder = (f"{task} is a {priority_message} task that requires immediate attention today !")
+        else:
+            reminder = (f"{task}is a {priority_message} task.Consider completing it when you have free time")
     case "medium":
-          reminder =(f"{task}is a medium priority task ")   
+        priority_message = "medium priority"
+        if time_bound =="yes":
+            reminder = (f"{task}is a {priority_message} task that requires  attention today !")
+        else:
+            reminder =  (f"{task}is a {priority_message} task.Consider completing it when you have free time")    
     case "low":
-          reminder = (f"{task} is a low priority task ")
-if time_bound == "yes":
-     reminder +=  "that requires immediate attention"
-else:
-     reminder += "Consider completing it when you have free time."     
+        priority_message = "low priority"
+        if time_bound == "yes":
+            reminder = (f"{task} is a {priority_message} task. Consider completing it when you have free time today")
+        else:
+              reminder =  (f"{task}is a {priority_message} task.Consider completing it when you have free time") 
 
-print(reminder)                 
+print(f"reminder:{reminder}")              
