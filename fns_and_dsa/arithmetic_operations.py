38ade20badb4b85c916a311833a85e68a6261e78
virginianjoki <njokivirginia0619@gmail.com>
2024-09-27 11:41:41 +0300

    arithmetic_operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,13 @@
+def perform_operation(num1, num2, operation):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiplication':
+        return num1 * num2
+    elif operation == 'division':
+        if num2 == 0:
+            return "Error: Division by zero is not allowed."
+        return num1 / num2
+    else:
+        return "Error: Invalid operation."
