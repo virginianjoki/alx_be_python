75e03f2d6b5e1bd38c2eaed330b5761d455a98f6
virginianjoki <njokivirginia0619@gmail.com>
2024-10-10 12:40:44 +0300

    oop

---
+++ oop/book_class.py
@@ -0,0 +1,17 @@
+class Book:
+    def __init__(self,tittle,author,year):
+        self.tittle = tittle
+        self.author = author
+        self.year = year
+  #deleting year
+
+    def __del__ (self):
+        self.tittle()
+        return "Deleting (tittle of the book)"
+  #string representation
+
+    def __str__(self):
+        return  f"{self.title} by {self.author}, published in {self.year}".
+    
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})".
