bd06bc1ed5e900e66dea7995f1acf2988e37bbcc
virginianjoki <njokivirginia0619@gmail.com>
2024-10-10 12:51:58 +0300

    oop

--- oop/book_class.py
+++ oop/book_class.py
@@ -7,10 +7,10 @@ class Book:
   #string representation
 
     def __str__(self):
-        return  f"{self.title} by {self.author}, published in {self.year}".
+        return  f"{self.title} by {self.author}, published in {self.year}"
     
     def __repr__(self):
-        return f"Book('{self.title}', '{self.author}', {self.year})".
+        return f"Book('{self.title}', '{self.author}', {self.year})"
     
   #deleting
     def __del__(self):
