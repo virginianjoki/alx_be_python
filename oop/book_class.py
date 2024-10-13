ccda91ed241cbdc8513e2d469ac6ea5d1ef6adce
virginianjoki <njokivirginia0619@gmail.com>
2024-10-13 21:45:32 +0300

    book_class

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,18 +1,18 @@
 class Book:
-    def _init_(self, title, author, year):
+    def __init__(self, title, author, year):
         #Initialize a Book instance with title, author, and year.
         self.title = title
         self.author = author
         self.year = year
 
-    def _del_(self):
+    def __del__(self):
         #Destructor method that prints a message when a Book instance is deleted.
         print(f"Deleting {self.title}")
 
-    def _str_(self):
+    def __str__(self):
         #String representation of the Book instance for human-readable format.
         return f"{self.title} by {self.author}, published in {self.year}"
 
-    def _repr_(self):
+    def __repr__(self):
         #String representation of the Book instance for debugging purposes.
         return f"Book('{self.title}', '{self.author}', {self.year})"
